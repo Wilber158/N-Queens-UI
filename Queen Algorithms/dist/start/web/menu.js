000bdf9b84a3293a73edb6bb8ecc4679d1320ab2
Wilber Cortez <Wilbercortez2013@gmail.com>
2023-04-29 23:29:58 -0400

    Added .exe

--- Queen Algorithms/dist/start/web/menu.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("annealing").addEventListener("click", function(){
-    window.location.href = "annealing.html"
-})
-
-document.getElementById("backtracking").addEventListener("click", function(){
-    window.location.href = "nqueens.html"
-})
